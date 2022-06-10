849f3c063d235c7c8aeeb85fba2b91de0a12f955
mailynmailyn <mlpuittinen@gmail.com>
2022-06-10 08:06:44 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path= "/thepaintstrokes.github.io" element={<HomePage />} /> 
-        <Route path='/thepaintstrokes.github.io/homepage' element={<HomePage />} /> 
+        <Route path= "" element={<HomePage />} /> 
+        <Route path='/homepage' element={<HomePage />} /> 
         <Route path='/thepaintstrokes.github.io/aboutus' element={<AboutUs />} />
         <Route path='/thepaintstrokes.github.io/workshopselect' element={<WorkshopSelection />} />
         <Route path= '/thepaintstrokes.github.io/artists' element={<Artists />} />
