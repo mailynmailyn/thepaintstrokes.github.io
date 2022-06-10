5dbf625f5e2bfd75c16d003de90cd0ec0df6f3da
mailynmailyn <mlpuittinen@gmail.com>
2022-06-10 08:40:11 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -6,14 +6,23 @@ import HomePage from './HomePage';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { HashRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>
+  , document.getElementById('root'));
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
