1f1d97e4d9bdd2faa406e786e7ee40ec11b22244
mailynmailyn <mlpuittinen@gmail.com>
2022-06-10 08:23:55 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -11,14 +11,16 @@ import Confirmation from './Confirmation';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Switch
 } from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
+    <Switch>
+    {/* <Router>
+      <Routes> */}
         <Route path= '/thepaintstrokes.github.io' element={<HomePage />} /> 
         <Route path='/thepaintstrokes.github.io/homepage' element={<HomePage />} /> 
         <Route path='/thepaintstrokes.github.io/aboutus' element={<AboutUs />} />
@@ -27,8 +29,9 @@ function App() {
         <Route path= '/thepaintstrokes.github.io/date' element={<RegistrationDate />} />
         <Route path= '/thepaintstrokes.github.io/userinfo' element={<UserInfo />} />
         <Route path= '/thepaintstrokes.github.io/confirmation' element={<Confirmation />} />
-      </Routes>
-    </Router>
+      {/* </Routes>
+    </Router> */}
+    </Switch>
   );
 }
 
